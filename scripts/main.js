d5f1f2ee4f75db74f5207cc0d7410f4cba4ddf4d
Daria Brandenburg <daria.dburg@gmail.com>
2021-07-09 04:07:42 +0200

    Initial commit

---
+++ scripts/main.js
@@ -0,0 +1,17 @@
+//Metrics
+function gravityConverter () {
+    alert("Earth’s gravity is 2.66 times that of Mars.");
+}
+
+function marsMilesConverter() {
+    alert("1 Mars mile is equal to 0,5 miles of Earth.");
+}
+
+//Environment
+function currentGravity() {
+    alert("The gravity is at 90% at the moment.");
+}
+
+function distanceTravelled() {
+    alert("23,999 km travelled so far.");
+}
